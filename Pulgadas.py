0c6b1513825ddf2c935c3b7c74cf2ba6a5a79a15
sebospi <jsospinab1@academia.usbbog.edu.co>
2023-08-29 15:33:00 -0500

    inicio

---
+++ Pulgadas.py
@@ -0,0 +1,3 @@
+n= int(input("Ingrese la medida en pulgadas"))
+r= n*25.4
+print(n,"pulgada(s) esquivale a", r,"milimetro(s)")
---
+++ abc.py
@@ -0,0 +1,16 @@
+
+n= input("Ingrese una letra")
+vocal1 = "a"; vocal2="e" ; vocal3="i"; vocal4="o";vocal5= "u"
+
+if n == vocal1:  
+   print("Es una vocal")
+elif n == vocal2:  
+   print("Es una vocal")
+elif n == vocal3:  
+   print("Es una vocal")
+elif n == vocal4:  
+   print("Es una vocal")
+elif n == vocal5:  
+   print("Es una vocal")
+else:
+   print("es una consonante")
