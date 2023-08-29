824e342535a124921bdff35502ac79146eb61846
sebospi <143553515+sebospi@users.noreply.github.com>
2023-08-29 16:50:13 -0500

    Add files via upload

---
+++ tercera clase/IVA.py
@@ -0,0 +1,3 @@
+n= int(input("Ingrese el valor del producto"))
+r= n + (n*0.19)
+print("El valor del producto con el IVA es de", r)
---
+++ tercera clase/Temperatura.py
@@ -0,0 +1,3 @@
+n= float(input("Ingrese la temperatura en °F"))
+r= (n-32)/1.8
+print(n, "°F equivalen a ", r,"°C")
